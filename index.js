83382267cc543b5314a20bbb647a966ec5195c63
BeyondSolar <abhicsjuet@gmail.com>
2023-10-11 14:16:05 +0530

    Added Increment, Save & Clear functionality for the Passenger COunter Tool

--- index.js
+++ index.js
@@ -6,3 +6,19 @@ function increment(){
     console.log("Button CLicked");
     count_el.innerText=count;
 }
+
+curr_count='Previous Entries - ';
+
+function save(){
+    curr_count+=(count+' - ');
+    count=0;
+    // count_el.textContent=count;
+    document.getElementById("save").innerText=curr_count;
+}
+
+function _clear(){
+    count=0;
+    curr_count='Previous Entries - '+count;
+    console.log("Button CLicked");
+    document.getElementById("save").innerText=curr_count;
+}
